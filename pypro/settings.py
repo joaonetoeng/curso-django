e408b411582581caae036f0e3676b0842b064b74
joaonetoeng <joaonetu@live.com>
2020-06-20 21:15:11 -0300

    Setado Variavel Debug no Settings 2

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-gm9467n)!4sk#&^ls-(s!o020m-xan2rbl@mf-pchf@k^=h#t'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
